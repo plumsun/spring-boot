8e465b86ae4fdb29079853c5339e2f3110fca1a2
lihaohan <plumsun@163.com>
2022-05-29 23:21:48 +0800

    过滤文件

---
+++ sdkdemo/src/test/java/com/example/sdkdemo/SdkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
