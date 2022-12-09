44a369eeb30c2f64953cf64ddc4269e9a07c0d96
lihaohan <plumsun@163.com>
2022-12-09 14:29:52 +0800

    新项目搭建

---
+++ api-demo/src/main/java/com/demo/App.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiHaoHan
+ * @date 2022/12/9
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
