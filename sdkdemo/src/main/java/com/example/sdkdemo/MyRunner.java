e90f78a943a9353bf65b8569f65c0a530ecb6049
lihaohan <plumsun@163.com>
2023-09-18 15:43:07 +0800

    sdk项目添加spring-mail支持

--- sdkdemo/src/main/java/com/example/sdkdemo/MyRunner.java
+++ sdkdemo/src/main/java/com/example/sdkdemo/MyRunner.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MyRunner implements ApplicationRunner {
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println(" 初始化完毕");
