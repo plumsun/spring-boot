163bd74ff1d4740b9fe47f781682fb276e0a30e9
lihaohan <plumsun@163.com>
2023-09-26 10:41:17 +0800

    gradle对接mybatis模板项目

--- gradle-demo/src/test/java/com/demo/redis/UserMapperTest.java
+++ gradle-demo/src/test/java/com/demo/redis/UserMapperTest.java
@@ -1,17 +1,22 @@
 package com.demo.redis;
 
 
-import com.demo.App;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * @date: 2023/7/5
  * @author: LiHaoHan
  */
-@SpringBootTest(classes = App.class)
-@RunWith(SpringRunner.class)
+// @SpringBootTest(classes = App.class)
+// @RunWith(SpringRunner.class)
 public class UserMapperTest {
 
+    @Test
+    public void Test(){
+        String str = "s,s.2.s,seg";
+        String[] split = str.split(",");
+        System.out.println("split = " + Arrays.toString(split));
+    }
 }
